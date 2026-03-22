6ca76059609f12d4c2a0af95cb178d83db944c70
Gabor <gabor.nemeth.de@gmail.com>
2026-03-23 00:04:50 +0100

    animáció #1

---
+++ 05-animáció/css/transition/main.js
@@ -0,0 +1,45 @@
+let token = document.querySelector('#token')
+let _dx = 0, _dy = 0
+const _delta = 50
+
+function positionToken() {
+    token.style.left = ((token.parentElement.clientWidth - token.clientWidth) >> 1) + 'px'
+    token.style.top = ((token.parentElement.clientHeight - token.clientHeight) >> 1) + 'px'
+}
+
+function openMenu(span) {
+    span.parentElement.querySelector('ul').classList.toggle('closed')
+}
+
+function move(e) {
+    switch (e.target.id) {
+        case 'left': _dx -= _delta; break
+        case 'right': _dx += _delta; break
+        case 'up': _dy -= _delta; break
+        case 'down': _dy += _delta; break
+    }
+
+    token.style.transform = `translate(${_dx}px, ${_dy}px)`
+}
+
+positionToken()
+
+window.onresize = positionToken
+document.querySelectorAll('button').forEach(btn => btn.addEventListener('click', move))
+const colors = ['red', 'green', 'blue']
+document.querySelectorAll('ball')
+    .forEach((b, ix) => {
+        b.style.left = (80 + 120*ix) + 'px';
+        b.style.backgroundColor = colors[ix];
+        b.addEventListener(
+            'click',
+            e => {
+                // let shadow = document.querySelector('.shadow')
+                e.target.classList.add('bounce')
+                // shadow.classList.add('small')
+                setTimeout(() => {
+                    e.target.classList.remove('bounce')
+                    // shadow.classList.remove('small')
+                }, 600)
+            })
+    })
