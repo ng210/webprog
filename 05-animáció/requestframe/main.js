8deb25509fa61eee9945e1d2013636d979e4c350
ng210 <gabor.nemeth.de@gmail.com>
2026-04-13 10:06:19 +0200

    requestanimationframe #2

--- 05-animáció/requestframe/main.js
+++ 05-animáció/requestframe/main.js
@@ -10,22 +10,26 @@ function moveBox1() {
     function animate(timestamp) {
         // ha az animáció először fut le,
         // startTime legyen a timestamp értéke
+        if (startTime == -1) {
+            startTime = timestamp
+        }
 
         // az eltelt idő = timestamp és startTime különbsége
-        let elapsed
+        let elapsed = timestamp - startTime
         // az animáció előrehaladása = eltelt idő és az időtartam hányadosa
-        let progress
+        let progress = elapsed / duration
 
         // ha az előrehaladás nagyobb mint 1,
         // azaz az animáció véget ért
         if (progress >= 1) {
             // kezdődjön újra
+            startTime = -1
         } else {
             // különben az aktuális x koordináta legyen
             // kezdeti pozíció + elmozdulás * előrehaladás
-            let currentX = startX
+            let currentX = startX + (endX - startX) * progress
             // a transform segítségével mozgassuk a dobozt az x tengelyen
-            box.style.transform = ''
+            box.style.transform = `translateX(${currentX}px)`
         }
         requestAnimationFrame(animate)
     }
@@ -36,9 +40,39 @@ function moveBox1() {
 function moveBox2() {
     const box = document.querySelector('#box2')
 
-    function animate(timestamp) {
+    const duration = 2000   // időtartam 2 sec
+    let startX = -100     // kezdő pozíció
+    let endX = 100        // vég pozíció
 
-        
+    let startTime = -1
+    
+    function animate(timestamp) {
+        // ha az animáció először fut le,
+        // startTime legyen a timestamp értéke
+        if (startTime == -1) {
+            startTime = timestamp
+        }
+
+        // az eltelt idő = timestamp és startTime különbsége
+        let elapsed = timestamp - startTime
+        // az animáció előrehaladása = eltelt idő és az időtartam hányadosa
+        let progress = elapsed / duration
+
+        // ha az előrehaladás nagyobb mint 1,
+        // azaz az animáció véget ért
+        if (progress >= 1) {
+            // kezdődjön újra
+            startTime = -1
+            tmp = startX
+            startX = endX
+            endX = tmp
+        } else {
+            // különben az aktuális x koordináta legyen
+            // kezdeti pozíció + elmozdulás * előrehaladás
+            let currentX = startX + (endX - startX) * progress
+            // a transform segítségével mozgassuk a dobozt az x tengelyen
+            box.style.transform = `translateX(${currentX}px)`
+        }
         requestAnimationFrame(animate)
     }
 
